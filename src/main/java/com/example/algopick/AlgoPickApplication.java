bd49b9e32152c2a8b8be82b089e32889542edf8c
99hyeon <hyeon99@ajou.ac.kr>
2025-02-18 19:30:39 +0900

    setting

---
+++ src/main/java/com/example/algopick/AlgoPickApplication.java
@@ -0,0 +1,13 @@
+package com.example.algopick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoPickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoPickApplication.class, args);
+    }
+
+}
