a265d7202b785a07505de694fd4526f252a2a8fe
99hyeon <hyeon99@ajou.ac.kr>
2025-02-25 17:03:38 +0900

    DB 연결
    DataDBConfig, MetaDBConfig 클래스 추가
    metadata저장할 db, 알고 문제들 저장할 db에 따로 연결하는 세팅함.

--- src/main/java/com/example/algopick/config/CorsConfig.java
+++ src/main/java/com/example/algopick/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.algopick;
+package com.example.algopick.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/com/example/algopick/entity/ProblemEntity.java
@@ -0,0 +1,19 @@
+//package com.example.algopick.entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Entity
+//@Getter
+//@Setter
+//public class ProblemEntity {
+//
+//    @Id
+//    private Integer id;
+//    private String category;
+//    private boolean retry;
+//    private boolean solved;
+//
+//}
